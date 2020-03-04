f8ac917c7689809a783626dd375580566a711130
Justin Zwick <jaz2130@columbia.edu>
2020-03-03 20:59:58 -0500

    styling is now done through bootstrap

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,13 @@
 class WelcomeController < ApplicationController
 
 	def show
-
+		# user shouldn't be looking at the log in page
+		# when they are already signed in
+		if session[:user_id]
+			# TODO: change this redirect to the
+			# Posts page, once that MVC is added
+			redirect_to "/users/me"
+		end
 	end
 
 end
