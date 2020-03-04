7124951271deb2b500a156ed2fdce268b38c44cc
Justin Zwick <jaz2130@columbia.edu>
2020-03-04 17:42:32 -0500

    make the homepage sexy

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,8 @@
 class WelcomeController < ApplicationController
 	def show
-		# user shouldn't be looking at the log in page
-		# when they are already signed in
 		if current_user
-			# TODO: change this redirect to the
-			# Posts page, once that MVC is added
-			redirect_to "/users/me"
+			@tutor_searches = TutorSearch.all
+			@tutee_searches = TuteeSearch.all
 		end
 	end
 end
